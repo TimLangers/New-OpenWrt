c4be12a0bc8943b1bc08103c89073539c38ebe0e
TimLangers <yangcui2049@163.com>
2026-06-16 21:44:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# 只负责添加源
-echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
+# ==========================================================
+# DIY-PART1.SH - 仅添加必要的插件源
+# ==========================================================
+
+# 只保留 OpenClash 源
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 
 # 更新与安装
