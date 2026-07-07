dc2d284259c200f8eea8b5a1a83b424d93c5247c
TimLangers <yangcui2049@163.com>
2026-07-07 22:26:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,9 +3,5 @@
 # DIY-PART1.SH - 仅添加必要的插件源
 # ==========================================================
 
-# 只保留 OpenClash 源
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
-
-# 更新与安装
-./scripts/feeds update -a
-./scripts/feeds install -a
+# 添加 OpenClash 源码到 feeds 配置文件末尾
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
